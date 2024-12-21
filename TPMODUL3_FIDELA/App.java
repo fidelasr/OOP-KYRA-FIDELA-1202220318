7bf7c096e679e41bee94b48194aeb671c09b9e1a
fidelasr <148118664+fidelasr@users.noreply.github.com>
2024-12-21 15:25:53 +0700

    Add files via upload

---
+++ TPMODUL3_FIDELA/App.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 250, 3o);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("My JavaFX App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
